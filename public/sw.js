308e53690f0da1df740638e6ae4a28143e561d19
Leo <leonzkyhv@gmail.com>
2020-07-28 18:29:46 -0700

    Added service worker

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,31 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('first-app')
-      .then(function(cache) {
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/css/app.css',
-          '/src/js/app.js'
-        ])
-      })
-  );
+self.addEventListener('install', function (event) {
+  console.log("[Service worked]  installig service worked ", event)
+  // event.waitUntil(
+  //   caches.open('first-app')
+  //     .then(function (cache) {
+  //       cache.addAll([
+  //         '/',
+  //         '/index.html',
+  //         '/src/css/app.css',
+  //         '/src/js/app.js'
+  //       ])
+  //     })
+  // );
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', function (event) {
+  console.log("[Service worked]  activate service worked ", event)
+  return self.clients.claim();
+})
+
+self.addEventListener('fetch', function (event) {
+  console.log("[Service worked]  fetching something ", event)
   event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        return res;
-      })
+    fetch(event.request)
+    //   caches.match(event.request)
+    //     .then(function (res) {
+    //       return res;
+    //     })
   );
 });
