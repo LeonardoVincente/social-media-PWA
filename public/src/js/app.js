bcdc6a4a872095d8b47e98b7f365cbfb395c4468
Leo <leonzkyhv@gmail.com>
2020-07-29 19:16:48 -0700

    Added xhr and fetch with post

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,29 +2,52 @@ var defferedPrompt;
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
-        .then(function(){
+        .then(function () {
             console.log('Service worker registered!');
         })
-        .catch(function(err){
+        .catch(function (err) {
             console.log("Error in service worked")
         });
 }
 
-window.addEventListener('beforeinstallpropmpt', function(){
+window.addEventListener('beforeinstallpropmpt', function () {
     console.log("Before install propmt fired");
     event.preventDefault();
     defferedPrompt = event;
     return false;
 });
 
-fetch('https://httpbin.org/ip')
-    .then(function(response){
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
+xhr.onload = function () {
+    console.log('response', xhr.response);
+}
+
+xhr.onerror = function () {
+    console.log('error');
+}
+
+xhr.send();
+
+fetch('https://httpbin.org/post', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    mode: 'cors', // no-cors
+    body: JSON.stringify({
+        message: 'Does this work?'
+    })
+})
+    .then(function (response) {
         console.log(response);
         return response.json();
     })
-    .then(function(data){
+    .then(function (data) {
         console.log(data);
     })
-    .catch(err=>{
+    .catch(err => {
         console.llg(err);
     })
