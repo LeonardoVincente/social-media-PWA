5092570868bb2cbb77bacae9adf5c0d39d81a7bb
Leo <leonzkyhv@gmail.com>
2020-07-30 15:39:41 -0700

    Clean up code and add polfill

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,9 @@
 var defferedPrompt;
+
+if(!window.Promise){
+    window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
@@ -16,38 +21,3 @@ window.addEventListener('beforeinstallpropmpt', function () {
     defferedPrompt = event;
     return false;
 });
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://httpbin.org/ip');
-xhr.responseType = 'json';
-xhr.onload = function () {
-    console.log('response', xhr.response);
-}
-
-xhr.onerror = function () {
-    console.log('error');
-}
-
-xhr.send();
-
-fetch('https://httpbin.org/post', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    mode: 'cors', // no-cors
-    body: JSON.stringify({
-        message: 'Does this work?'
-    })
-})
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
-    .catch(err => {
-        console.llg(err);
-    })
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,4 @@
 self.addEventListener('install', function (event) {
-  console.log("[Service worked]  installig service worked ", event)
   // event.waitUntil(
   //   caches.open('first-app')
   //     .then(function (cache) {
@@ -15,12 +14,10 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('activate', function (event) {
-  console.log("[Service worked]  activate service worked ", event)
   return self.clients.claim();
 })
 
 self.addEventListener('fetch', function (event) {
-  console.log("[Service worked]  fetching something ", event)
   event.respondWith(
     fetch(event.request)
     //   caches.match(event.request)
