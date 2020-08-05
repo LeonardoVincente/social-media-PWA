5f0750d91bcaf1b7993994a8dea0bfc688b69452
Leo <leonzkyhv@gmail.com>
2020-08-05 09:22:44 -0700

    responding wotj cache for static

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,28 @@
-const CACHE_STATIC_NAME = 'static-v16';
+const CACHE_STATIC_NAME = 'static-v17';
 const CACHE_DYNAMIC_NAME = 'dynamic-v2';
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  'src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME)
       .then(function (cache) {
         console.log('[Service Worker] Precaching app shell');
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/offline.html',
-          '/src/js/app.js',
-          'src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+        cache.addAll(STATIC_FILES);
       })
   );
   return self.clients.claim();
@@ -111,6 +112,10 @@ self.addEventListener('fetch', function (event) {
             })
         })
     )
+  } else if (new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url)) {
+    event.respondWith(
+      caches.match(event.request)
+    )
   } else { // cache with network fallback
     event.respondWith(
       caches.match(event.request)
@@ -131,7 +136,7 @@ self.addEventListener('fetch', function (event) {
                 console.log(err)
                 return caches.open(CACHE_STATIC_NAME)
               }).then(function (cache) {
-                if(event.request.url.indexOf('/help') > -1){
+                if (event.request.url.indexOf('/help') > -1) {
                   return cache.match('/offilne.html');
                 }
               })
